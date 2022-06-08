b27a587d02173008c2663e07a7d1512299cb4d5e
IsacHolstad <wisac22@gmail.com>
2022-06-09 00:37:13 +0200

    getting error

--- js/cam.js
+++ js/cam.js
@@ -1,1 +1,11 @@
-const cameraBox = document.querySelector('video');
+var video = document.querySelector('video');
+
+const options = {
+    video:true
+
+}
+
+navigator.mediaDevices.getUserMedia(options);
+then(function(stream){
+    video.srcObject = stream
+});
